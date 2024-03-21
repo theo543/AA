ae715d1a1943b40e229bc3b706b2f52f15c0bab8
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-03-21 19:28:22 +0200

    Add extreme verbosity for first generation, also fixed crossover bug

--- genetic/genetic_polynomial_max.py
+++ genetic/genetic_polynomial_max.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from argparse import ArgumentParser
 from json import loads
-from math import ceil, log2
+from math import ceil, log2, log10
 from random import Random
 from itertools import accumulate
 from typing import Any
@@ -73,14 +73,14 @@ def mutate(c: Chromosome, bit_flip_chance: float, rng: Random, d: Discretize) ->
             bits[i] = '1' if (bits[i] == '0') else '0'
     return d.decode_chromo(''.join(bits))
 
-def crossover(a: Chromosome, b: Chromosome, rng: Random, d: Discretize) -> tuple[Chromosome, Chromosome]:
+def crossover(a: Chromosome, b: Chromosome, rng: Random, d: Discretize) -> tuple[Chromosome, Chromosome, int]:
     bits_a = a.encoded
     bits_b = b.encoded
     assert len(bits_a) == len(bits_b)
     cut = rng.randint(0, len(bits_a))
     new_a = bits_a[:cut] + bits_b[cut:]
     new_b = bits_b[:cut] + bits_a[cut:]
-    return d.decode_chromo(new_a), d.decode_chromo(new_b)
+    return d.decode_chromo(new_a), d.decode_chromo(new_b), cut
 
 @dataclass
 class SelectionData:
@@ -103,6 +103,8 @@ def select_chromosomes(amount_to_select: int, population: list[Chromosome], cumu
         u = rng.random()
         chosen = search(u, cumulative_prob)
         selected.append(population[chosen])
+        if verbose:
+            print(f"u={u}: Select chromosome {chosen}")
     return selected
 
 def search(item: Any, lst: list[Any]):
@@ -126,6 +128,9 @@ def generate_population(amount: int, rng: Random, d: Discretize) -> list[Chromos
         return f"{nr:0{bits}b}"
     return [d.decode_chromo(rand_bits()) for _ in range(amount)]
 
+def format_chromosome(c: Chromosome, poly: Polynomial) -> str:
+    return f"(encoded={c.encoded}, value={c.value}, fitness={poly.eval(c.value)})"
+
 def main():
     cfg = parse_args()
     print(cfg)
@@ -133,41 +138,93 @@ def main():
     d = Discretize(cfg.domain_start, cfg.domain_end, cfg.decimal_precision)
     rng = Random(cfg.random_seed)
     population = generate_population(cfg.population_size, rng, d)
-    verbose = True #TODO print loads of stuff on first iteration
-    #TODO print initial population
+    verbose = True
+    print("Generation 0 (initial population):")
+    idx_pad = ceil(log10(len(population)))
+    def pad(index: int) -> str:
+        return f"{index + 1: >{idx_pad}}"
+    for i, c in enumerate(population):
+        print(f"{pad(i)}: {format_chromosome(c, poly)}")
+    print()
     for i in range(cfg.generations):
-        print(f"Generation {i + 1}")
-        s_data = select_gen_data(poly, population)
-        #TODO print prob and cumulative prob
         fitnesses = [poly.eval(c.value) for c in population]
-        avg_fitness = sum(fitnesses) / len(fitnesses)
         best_fitness_index = max(range(len(fitnesses)), key=fitnesses.__getitem__)
-        best_fitness = fitnesses[best_fitness_index]
         best = population[best_fitness_index]
-        print(f"Average fitness: {avg_fitness}")
-        print(f"Best fitness: {best_fitness}")
         amount_to_select = len(population)
+        print(f"Best chromosome: {format_chromosome(best, poly)}")
         if cfg.copy_best_to_new_generation:
             amount_to_select -= 1
+        
+        s_data = select_gen_data(poly, population)
+        if verbose:
+            print()
+            print("Selection probabilities:")
+            for i, p in enumerate(s_data.prob):
+                print(f"Chromosome {pad(i)}: P={p}")
+            print("Cumulative probabilities: [" + ', '.join(f"{p}" for p in s_data.cumulative_prob) + "]")
         selected = select_chromosomes(amount_to_select, population, s_data.cumulative_prob, rng, verbose)
+        if verbose:
+            print()
+            print("Selected chromosomes:")
+            for i, c in enumerate(selected):
+                print(f"{pad(i)}: {format_chromosome(selected[i], poly)}")
+
         to_crossover: list[int] = []
-        #TODO print crossover process
+        if verbose:
+            print()
+            print(f"Crossover probability = {cfg.crossover_chance}")
         for i in range(len(selected)):
             u = rng.random()
             if u <= cfg.crossover_chance:
                 to_crossover.append(i)
+            if verbose:
+                print(f"{pad(i)}: {selected[i].encoded} u={u} ", end='')
+                if u <= cfg.crossover_chance:
+                    print(f" | Will crossover" if (u <= cfg.crossover_chance) else "")
+                else:
+                    print()
         rng.shuffle(to_crossover)
         if len(to_crossover) % 2 == 1:
-            to_crossover.pop()
+            c = to_crossover.pop()
+            if verbose:
+                print(f"Removed {c} from crossover list to have an even amount.")
+
         for i in range(0, len(to_crossover) - 1, 2):
-            selected[i], selected[i + 1] = crossover(selected[i], selected[i + 1], rng, d)
-        #before_mutation = selected
+            s = selected
+            x, y = to_crossover[i], to_crossover[i + 1]
+            if verbose:
+                print(f"Crossover {x} with {y}:")
+                print(f" {s[x].encoded} {s[y].encoded} ")
+            s[x], s[y], cut = crossover(s[x], s[y], rng, d)
+            if verbose:
+                print(f" Cut = {cut}")
+                print(f" {s[x].encoded}, {s[y].encoded}")
+
+        if verbose:
+            print()
+            print(f"Mutation probability = {cfg.mutation_chance}")
+        before_mutation = [c.encoded for c in selected]
         selected = [mutate(c, cfg.mutation_chance, rng, d) for c in selected]
+        diff = [i for i in range(len(selected)) if before_mutation[i] != selected[i].encoded]
+        if verbose:
+            print(f"Changed chromosomes: {diff}")
+            print("Chromosomes after mutation:")
+            for i, c in enumerate(selected):
+                print(f"{pad(i)}: {format_chromosome(c, poly)}")
+            print()
         if cfg.copy_best_to_new_generation:
             selected.append(best)
+            if verbose:
+                print(f"Added saved copy of best chromosome: {format_chromosome(best, poly)}")
         population = selected
-        verbose = False
-    #TODO print results as we go, and print final result
+        if verbose:
+            verbose = False
+            print()
+            print("Further generations will only print the best chromosome.")
+
+    final_best = max(population, key=lambda c: poly.eval(c.value))
+    print()
+    print(f"Best chromosome after {cfg.generations} generations: {format_chromosome(final_best, poly)}")
 
 if __name__ == "__main__":
     main()
